Frug/unix-history-repo

commit 6c6fda646f8ec4a27c70b146637bac0cbd364e49

Do not take the process lock. The assignment to u_short inside the properly aligned structure is atomic on all supported architectures, and the thread that should see side-effect of assignment is the same thread that does assignment.
Use a more appropriate conditional to detect the linux ABI.

Suggested by:	kib
X-MFC:		together with r215664


git-svn-id: svn+ssh://svn.freebsd.org/base/head@215675 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/compat/linux/linux_emul.c
@@ -198,11 +198,8 @@ linux_proc_exit(void *arg __unused, struct proc *p)
 	} else	
 		EMUL_SHARED_WUNLOCK(&emul_shared_lock);
 
-	if ((shared_flags & EMUL_SHARED_HASXSTAT) != 0) {
-		PROC_LOCK(p);
+	if ((shared_flags & EMUL_SHARED_HASXSTAT) != 0)
 		p->p_xstat = shared_xstat;
-		PROC_UNLOCK(p);
-	}
 
 	if (child_clear_tid != NULL) {
 		struct linux_sys_futex_args cup;
@@ -265,7 +262,8 @@ linux_proc_exec(void *arg __unused, struct proc *p, struct image_params *imgp)
 	if (__predict_false(imgp->sysent == &elf_linux_sysvec
 	    && p->p_sysent != &elf_linux_sysvec))
 		linux_proc_init(FIRST_THREAD_IN_PROC(p), p->p_pid, 0);
-	if (__predict_false(p->p_sysent == &elf_linux_sysvec))
+	if (__predict_false((p->p_sysent->sv_flags & SV_ABI_MASK) ==
+	    SV_ABI_LINUX))
 		/* Kill threads regardless of imgp->sysent value */
 		linux_kill_threads(FIRST_THREAD_IN_PROC(p), SIGKILL);
 	if (__predict_false(imgp->sysent != &elf_linux_sysvec

commit 06c60b553539cd743de4ff5e72e1904179d05d98

Fix kernel build with MFC of r215706:   Fix linux kernel module breakage introduced in r215675, by including   <sys/sysent.h>.
Pointyhat to:	netchild
Noticed by:	kib

sys/compat/linux/linux_emul.c
@@ -41,6 +41,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/sx.h>
 #include <sys/proc.h>
 #include <sys/syscallsubr.h>
+#include <sys/sysent.h>
 #include <sys/sysproto.h>
 #include <sys/unistd.h>
 